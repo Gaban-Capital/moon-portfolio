ad83c178cae5b98a6df8aff3776c816a680a20ac
leongaban <leongaban@gmail.com>
2023-10-14 14:41:29 -0500

    adding button styles

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
-    <main className="flex">
-      <h1>Hello world - page</h1>
+    <main className="mt-[100px]">
+      <div className="max-w-[335px]">
+        <h1 className="text-pink font-bold text-4xl">Moon Portfolio</h1>
+        <h4 className="text-yellow text-2xl	">
+          Track and project your crypto wealth
+        </h4>
+        <button type="button" className="btn gray-333 w-full mb-4">
+          Sign Up with Email
+        </button>
+        <button type="button" className="btn gray-555 w-full">
+          Continue with Google
+        </button>
+      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,44 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  corePlugins: {
+    // due to https://github.com/tailwindlabs/tailwindcss/issues/6602 - buttons disappear
+    preflight: false,
+  },
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      brown: "#261C15",
+      yellow: "#EEE0CB",
+      green: "#4E6E58",
+      pink: "#E56399",
+      red: "##D10000",
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
       },
     },
+    fontFamily: {
+      sans: ["Space_Grotesk"],
+      serif: ["Noto_Serif"],
+    },
+    extend: {},
   },
   plugins: [],
-}
-export default config
+};
+export default config;

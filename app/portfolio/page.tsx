44658d3da9426bd476df5c13bc7dcbcc03da2ebe
leongaban <leongaban@gmail.com>
2023-10-19 10:32:38 -0500

    added example posts page with fetch

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,9 +1,34 @@
-import { FC } from 'react';
+'use client';
+
+import { FC, useEffect, useState } from 'react';
 
 interface pageProps {}
 
+// type Post = {
+//   id: number;
+//   title: string;
+//   body: string;
+//   userId: number;
+//   tags: [string];
+//   reactions: number;
+// };
+
+// type Posts = {
+//   posts: [Post];
+// };
+
+async function getPosts() {
+  let res = await fetch('https://dummyjson.com/posts?limit=3');
+  return res.json();
+}
+
 const page: FC<pageProps> = ({}) => {
-  return <div className="user-auth-forms"></div>;
+  return (
+    <div className="portfolio-container">
+      <h1>Portfolio Container</h1>
+      <div></div>
+    </div>
+  );
 };
 
 export default page;

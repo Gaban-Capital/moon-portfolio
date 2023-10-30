e5b090087c75e4fba09a2e8947b7b7d3cedbaa36
leongaban <leongaban@gmail.com>
2023-10-30 13:56:57 -0500

    Search component prepped for Pyth logic

--- components/search.tsx
+++ components/search.tsx
@@ -1,7 +1,9 @@
-import React, { useState, ChangeEvent, useEffect } from 'react';
+import React, { ChangeEvent, useState, useEffect, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import SearchIcon from '@/components/search-icon';
 import { pluckCoinNames } from '@/common/utils/formatters';
+import { pythCoins } from '@/common/constants/pyth-coins';
 
 interface Coin {
   name: string;
@@ -20,10 +22,25 @@ const Search: React.FC<SearchProps> = ({
   valueToChange,
   coins,
 }) => {
+  // TODO
+  // Error: could not find react-redux context value; please ensure the component is wrapped in a <Provider>
+  // const dispatch = useDispatch();
   const [inputValue, setInputValue] = useState('');
   const [coinNames, setCoinNames] = useState(['']);
+  const [filter, setFilter] = useState('');
 
   console.log('coins', coins);
+
+  const handleCoinClick = (coin: any) => {};
+
+  const filteredCoins = useMemo(() => {
+    if (!filter) return pythCoins;
+    return pythCoins.filter(
+      coin =>
+        coin.symbol.toLowerCase().indexOf(filter.toLowerCase()) !== -1 ||
+        coin.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1
+    );
+  }, [filter]);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const newValue = event.target.value;
@@ -31,6 +48,10 @@ const Search: React.FC<SearchProps> = ({
     onInputChange(newValue);
     setCoinNames(pluckCoinNames(coins));
   };
+
+  // TODO
+  // Goal when user is searching, send what they type to filter out coins via the pythCoins.ts
+  // Then once a coin is selected return a coin object with it's current price
 
   useEffect(() => {
     if (valueToChange === '') {
